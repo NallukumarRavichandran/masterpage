720bb6c232abf4cc11fa21e0bce91f52331b61af
NallukumarRavichandran <nallukumargithub@gmail.com>
2025-12-05 12:43:39 +0530

    Updated Now

--- masterpage/Site1.Master.designer.cs
+++ masterpage/Site1.Master.designer.cs
@@ -33,15 +33,6 @@ namespace masterpage
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-
-        /// <summary>
         /// MasterContent control.
         /// </summary>
         /// <remarks>
